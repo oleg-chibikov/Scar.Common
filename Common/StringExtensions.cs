06d1933f11de1b7281d3fb10224331c17278f9ea
Oleg Chibikov <ochibikov@gmail.com>
2019-03-20 23:28:43 +1100

    Sync, Sync.Windows packages
    Small fixes

--- Common/StringExtensions.cs
+++ Common/StringExtensions.cs
@@ -2,15 +2,20 @@ namespace Scar.Common
 {
     public static class StringExtensions
     {
-        public static string? Capitalize(this string? str)
+        public static string? CapitalizeIfNotEmpty(this string? str)
         {
-            str = str?.Trim();
-            if (str != null && str.Length > 1)
+            return string.IsNullOrWhiteSpace(str) ? str : Capitalize(str);
+        }
+
+        public static string Capitalize(this string str)
+        {
+            str = str.Trim();
+            if (str.Length > 1)
             {
-                return char.ToUpper(str[0]) + str.Substring(1);
+                return char.ToUpperInvariant(str[0]) + str.Substring(1);
             }
 
-            return str?.ToUpper();
+            return str.ToUpperInvariant();
         }
     }
 }
