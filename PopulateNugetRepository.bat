dab91134086e9ec4123cb2324f9fff635d23cb21
Oleg Chibikov <oleg.chibikov@wisetechglobal.com>
2020-04-15 19:29:44 +1000

    changed nuget repository

--- PopulateNugetRepository.bat
+++ PopulateNugetRepository.bat
@@ -1,4 +1,4 @@
 @echo off
 
 del /s /q "Nuget\*.*"
-for /R "packages" %%I in (Scar.*nupkg) do copy /-y "%%I" Nuget
+for /R "%userprofile%\.nuget\packages" %%I in (Scar.*nupkg) do copy /-y "%%I" Nuget
